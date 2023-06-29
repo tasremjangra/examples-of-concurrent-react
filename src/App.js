759280212ebd58754f165ff86a9840202663b2f2
Tarsem Jangra <tarsemjangra@Tarsems-MacBook-Pro.local>
2023-06-29 18:56:49 +0530

    new feature in react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './navigation/Navigation';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navigation />
+      </header>
+    </div>
+  );
+}
+
+export default App;
